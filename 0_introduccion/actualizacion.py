e9a9240cc96aacfa7579a5043a68963293aff955
Alejandro <lunaticolupin@outlook.com>
2021-09-30 19:41:30 -0500

    Delete y update

--- 0_introduccion/actualizacion.py
+++ 0_introduccion/actualizacion.py
@@ -7,19 +7,43 @@ cursor = cnx.cursor() # Recordset
 
 query = "insert into Cliente values (%s, %s, %s, %s)"
 
-valores = (None, 'Pedro', 'Dom conocido S/N', 'Cholula')
+valores = (None, 'PedroNN', 'Dom conocido S/NMMM', 'Cholula')
 
 try: 
   cursor.execute(query, valores)
 
   if cursor.rowcount:
     print("Registro creado")
-    cursor.close()
     cnx.commit()
 
+except Exception as e:
+  print(e)
+  cnx.rollback() 
+
+query =  "delete from Cliente where nombreCliente like %s"
+
+try:
+  cursor.execute(query, ['%Pedro%'])
+  if cursor.rowcount:
+    print("Registro eliminado")
+    cnx.commit()
+except Exception as e:
+  print(e)
+  cnx.rollback() 
+
+
+query = "update Cliente set nombreCliente = %s where id = %s "
+valores = ('Hugo', 3)
+
+try:
+  cursor.execute(query, valores)
+  if cursor.rowcount:
+    print("Registro actualizado")
+    cnx.commit()
 except Exception as e:
   print(e)
   cnx.rollback()
 
 
+cursor.close()
 cnx.close()
